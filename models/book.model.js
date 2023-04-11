ad5f16216a0d898750b3c416937fd580e5c76474
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-04-11 12:44:05 +0100

    mongoose model setup

---
+++ models/book.model.js
@@ -0,0 +1,22 @@
+// grab mongoose Schema method
+const { Schema } = require('mongoose');
+
+// define schema
+const bookSchema = Schema({
+  title: {type: String, required: true},
+  author: {type: String, required: true},
+  avatarUrl: {type: String, required: true},
+  feelings: {type: String, required: false},
+  characters: {type: String, required: false},
+  writingStyle: {type: String, required: false},
+  notLiked: {type: String, required: false},
+  mostEnjoyed: {type: String, required: false},
+  other: {type: String, required: false},
+  rating: {type: Schema.Types.Decimal128, required: true},
+})
+
+// create model
+const Book = mongoose.model('Book', bookSchema);
+
+// export model
+module.exports = Book;
